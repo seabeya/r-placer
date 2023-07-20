6799768c3ab0911596534079258d87aca156339e
shaanaliyev <35802638+shaanaliyev@users.noreply.github.com>
2023-07-20 21:35:03 +0400

    update: place page

--- assets/place.js
+++ assets/place.js
@@ -14,8 +14,11 @@
     // Get current mouse x,y positions:
     const getMousePos = (canvas, evt) => {
       const rect = canvas.getBoundingClientRect();
-      const x = ((evt.clientX - rect.left - 15) / (rect.right - rect.left)) * canvas.width;
-      const y = ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height;
+      const x =
+        ((evt.clientX - rect.left - 15) / (rect.right - rect.left)) *
+        canvas.width;
+      const y =
+        ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height;
       return { x, y };
     };
 
@@ -32,23 +35,23 @@
         }
       }
       // Grid line color:
-      ctx.strokeStyle = '#a7a7a7';
+      ctx.strokeStyle = "#a7a7a7";
 
       ctx.stroke();
     };
 
     // Creating Canvas: {
-    const canvas_bg = document.getElementById('canvas_bg');
-    const bg = canvas_bg.getContext('2d');
+    const canvas_bg = document.getElementById("canvas_bg");
+    const bg = canvas_bg.getContext("2d");
 
-    const canvas_fg = document.getElementById('canvas_fg');
-    const fg = canvas_fg.getContext('2d');
+    const canvas_fg = document.getElementById("canvas_fg");
+    const fg = canvas_fg.getContext("2d");
 
     const img = new Image();
     img.onload = () => {
       w = img.width * ONE_PIXEL;
       h = img.height * ONE_PIXEL;
-      document.title = 'R-Placer ' + img.width * img.height + ' pixels';
+      document.title = "R-Placer " + img.width * img.height + " pixels";
 
       bg.canvas.width = w + X_OFFSET;
       bg.canvas.height = h + 2 * Y_OFFSET;
@@ -60,24 +63,24 @@
       drawGrid(0, Y_OFFSET, w, h + Y_OFFSET, bg);
     };
     img.onerror = () => {
-      location.href = 'https://shaanaliyev.github.io/r-placer/';
+      location.href = "https://shaanaliyev.github.io/r-placer/";
     };
     img.src = image;
     // }
 
     // Coordinates text:
     const drawText = (text, x, y) => {
-      fg.font = '40px Sans-serif';
+      fg.font = "40px Sans-serif";
       // black overlay:
-      fg.strokeStyle = 'black';
+      fg.strokeStyle = "black";
       fg.lineWidth = 8;
       fg.strokeText(text, x, y);
       // white text:
-      fg.fillStyle = 'white';
+      fg.fillStyle = "white";
       fg.fillText(text, x, y);
     };
 
-    canvas_fg.addEventListener('mousemove', (event) => {
+    canvas_fg.addEventListener("mousemove", (event) => {
       let p = getMousePos(canvas_fg, event);
       let x = Math.floor(p.x / ONE_PIXEL);
       let y = Math.floor((p.y - Y_OFFSET) / ONE_PIXEL);
@@ -88,9 +91,14 @@
         Math.floor(p.x / ONE_PIXEL) + X_TOP_LEFT >= start_x &&
         Math.floor((p.y - Y_OFFSET) / ONE_PIXEL) + Y_TOP_LEFT >= start_y &&
         Math.floor(p.x / ONE_PIXEL) + X_TOP_LEFT < img.width + start_x &&
-        Math.floor((p.y - Y_OFFSET) / ONE_PIXEL) + Y_TOP_LEFT < img.height + start_y
+        Math.floor((p.y - Y_OFFSET) / ONE_PIXEL) + Y_TOP_LEFT <
+          img.height + start_y
       ) {
-        drawText('(' + (x + X_TOP_LEFT) + ', ' + (y + Y_TOP_LEFT) + ')', p.x + 50, p.y - 15);
+        drawText(
+          "(" + (x + X_TOP_LEFT) + ", " + (y + Y_TOP_LEFT) + ")",
+          p.x + 50,
+          p.y - 15
+        );
       }
 
       fg.lineWidth = 2;
@@ -98,23 +106,33 @@
       let circle_y = y * ONE_PIXEL + Y_OFFSET + ONE_PIXEL / 2;
 
       fg.beginPath();
-      fg.strokeStyle = 'white';
+      fg.strokeStyle = "white";
       fg.arc(circle_x, circle_y, ONE_PIXEL - 6, 0, 2 * Math.PI, false);
       fg.stroke();
 
       fg.beginPath();
-      fg.strokeStyle = 'black';
+      fg.strokeStyle = "black";
       fg.arc(circle_x, circle_y, ONE_PIXEL - 4, 0, 2 * Math.PI, false);
       fg.stroke();
     });
 
-    canvas_fg.addEventListener('click', (event) => {
+    canvas_fg.addEventListener("click", (event) => {
       let p = getMousePos(canvas_fg, event);
       let x = Math.floor(p.x / ONE_PIXEL) + X_TOP_LEFT;
       let y = Math.floor((p.y - Y_OFFSET) / ONE_PIXEL) + Y_TOP_LEFT;
-      if (x >= start_x && y >= start_y && x < img.width + start_x && y < img.height + start_y) {
-        let url = 'https://new.reddit.com/r/place/?cx=' + x + '&cy=' + y + '&px=12';
-        window.open(url, '_blank').focus();
+      if (
+        x >= start_x &&
+        y >= start_y &&
+        x < img.width + start_x &&
+        y < img.height + start_y
+      ) {
+        let url =
+          "https://new.reddit.com/r/place/?cx=" +
+          x +
+          "&cy=" +
+          y +
+          "&px=15&screenmode=fullscreen";
+        window.open(url, "_blank").focus();
       }
     });
   };
@@ -122,13 +140,13 @@
   // Run:
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const url = urlParams.get('url');
-  const x = urlParams.get('x');
-  const y = urlParams.get('y');
+  const url = urlParams.get("url");
+  const x = urlParams.get("x");
+  const y = urlParams.get("y");
 
   if (url && x && y) {
     place(url, +x, +y);
   } else {
-    location.href = 'https://shaanaliyev.github.io/r-placer/';
+    location.href = "https://shaanaliyev.github.io/r-placer/";
   }
 })();
