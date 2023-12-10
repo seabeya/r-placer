84502558063ccd5c8861112bc75ab9373f066ce2
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2023-12-10 19:01:32 +0400

    add: Vite Multi-Page (workspace)

---
+++ workspace/Workspace.tsx
@@ -0,0 +1,9 @@
+function Workspace() {
+  return (
+    <div className="flex h-screen items-center justify-center text-4xl text-[#FF4500]">
+      <p className="font-medium">Workspace</p>
+    </div>
+  );
+}
+
+export default Workspace;
---
+++ workspace/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Workspace from './Workspace.tsx';
+import '../src/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Workspace />
+  </React.StrictMode>,
+);
