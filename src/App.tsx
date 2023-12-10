0aecd7257a87a730858de9c4fdc73c2337d9e83d
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2023-12-10 16:27:30 +0400

    Getting started with Vite

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center text-4xl text-[#FF4500]">
+      <p className="font-medium">R-Placer</p>
+    </div>
+  );
+}
+
+export default App;
