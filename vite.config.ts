1a7aaff17b99b95e977d2a3e2d80327bf1edc150
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-01-18 23:03:19 +0400

    setup: Add Vite Multi-Page stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        workspace: resolve(__dirname, 'workspace/index.html'),
+      },
+    },
+  },
 });
---
+++ workspace/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-red-500">R-Placer Workspace</h1>
+    </>
+  );
+}
+
+export default App;
