d6e20c84a5e7e9aa837dcd5be9880bdeaa52b69a
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-01-18 22:37:48 +0400

    Add font Inter (Regular, Medium)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 };
