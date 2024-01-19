35714c14e46e4b7ad834f4018e43f5fe60fb484e
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-01-19 04:10:52 +0400

    Add global consts file

---
+++ src/global/consts.ts
@@ -0,0 +1,7 @@
+const CONSTS = {
+  main_page_url: (import.meta.env.VITE_MAIN_PAGE_URL as string) || 'https://r-placer.seabeya.com/',
+  workspace_page_url: (import.meta.env.VITE_WORKSPACE_PAGE_URL as string) || 'https://r-placer.seabeya.com/workspace/',
+  github_url: 'https://github.com/seabeya/r-placer',
+};
+
+export default CONSTS;
