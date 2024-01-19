2f42df4de09dbf8d403a39d7975135880a219bab
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-01-19 20:06:11 +0400

    Fix the missing of the type attribute on a button element

--- src/pages/root/components/parts/Button.tsx
+++ src/pages/root/components/parts/Button.tsx
@@ -8,6 +8,7 @@ type ButtonProps = {
 function Button({ children, isDisabled }: ButtonProps) {
   return (
     <button
+      type="submit"
       disabled={isDisabled}
       className="flex items-center gap-1 rounded-md bg-gray-300 px-8 py-2 text-sm text-black duration-75 hover:gap-2 hover:bg-gray-200 disabled:cursor-wait disabled:bg-gray-400 disabled:hover:gap-1 sm:text-base"
     >
