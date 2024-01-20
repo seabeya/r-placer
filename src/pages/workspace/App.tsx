c3a86ce511146227fb19e134c8e181f3a5a022d7
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-01-20 15:23:44 +0400

    Add canvas generation logic

--- src/pages/workspace/App.tsx
+++ src/pages/workspace/App.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useRef } from 'react';
+
 import Header from '@workspace/components/Header.tsx';
+import { Place } from '@workspace/Place.ts';
+
+import { checkInputs, getInputsFromUrl } from '@global/utils.ts';
+import CONSTS from '@global/consts.ts';
 
 function App() {
+  const canvasBack = useRef<HTMLCanvasElement>(null);
+  const canvasFront = useRef<HTMLCanvasElement>(null);
+
+  // Get data from URL:
+  const queryString = window.location.search;
+  const { url, x, y } = getInputsFromUrl(queryString);
+
+  useEffect(() => {
+    if (checkInputs(url, x, y).status) {
+      const place = new Place({
+        canvas_bg_element: canvasBack.current!,
+        canvas_fg_element: canvasFront.current!,
+        imgUrl: url,
+        start_x: x,
+        start_y: y,
+      });
+      place.generate();
+    } else {
+      location.href = CONSTS.main_page_url;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
-      <Header imageUrl="" />
-      <h1 className="text-5xl text-red-500">R-Placer Workspace</h1>
+      <Header imageUrl={url} />
+      <canvas ref={canvasBack} className="absolute left-0 top-0"></canvas>
+      <canvas ref={canvasFront} className="absolute cursor-none"></canvas>
     </>
   );
 }
