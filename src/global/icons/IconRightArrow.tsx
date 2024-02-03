d7533634e29418589474a70a23033cdde0bd57b8
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-03 17:55:52 +0400

    Remove `IconRightArrow` component and replace with arrow symbol

--- src/global/icons/IconRightArrow.tsx
+++
@@ -1,9 +0,0 @@
-function IconRightArrow({ className }: { className?: string }) {
-  return (
-    <svg fill="currentColor" viewBox="0 0 16 16" height="1em" width="1em" className={className}>
-      <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753l5.48-4.796a1 1 0 000-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 001.659.753z" />
-    </svg>
-  );
-}
-
-export default IconRightArrow;
--- src/pages/root/components/parts/Button.tsx
+++ src/pages/root/components/parts/Button.tsx
@@ -1,5 +1,3 @@
-import IconRightArrow from '@global/icons/IconRightArrow.tsx';
-
 type ButtonProps = {
   children: React.ReactNode;
   isDisabled: boolean;
@@ -13,7 +11,7 @@ function Button({ children, isDisabled }: ButtonProps) {
       className="flex items-center gap-1 rounded-md bg-gray-300 px-8 py-2 text-sm text-black duration-75 hover:gap-2 hover:bg-gray-200 disabled:cursor-wait disabled:bg-gray-400 disabled:hover:gap-1 sm:text-base"
     >
       {children}
-      <IconRightArrow className="h-5 w-5" />
+      <span>-&gt;</span>
     </button>
   );
 }
