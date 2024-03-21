662ab8672639a3f9a0cc6a0ee0f6017b0a89a4cc
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-03-21 16:46:48 +0400

    Add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,32 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: '',
-  description: '',
+  title: 'R-Placer',
+  description: 'Pixel Art Placer/Coordinator for the Reddit Place Event.',
+  keywords: [
+    'reddit place',
+    'pixel art',
+    'pixel',
+    'reddit event',
+    'overlay',
+    'reddit place tool',
+    'r-place',
+    'r-placer',
+    'seabeya',
+  ],
+  openGraph: {
+    type: 'website',
+    url: 'https://r-placer.seabeya.com',
+    title: 'R-Placer',
+    description: 'Pixel Art Placer/Coordinator for the Reddit Place Event.',
+    siteName: 'R-Placer',
+    images: [
+      {
+        url: 'https://r-placer.seabeya.com/icons/icon-96x96.png',
+        alt: 'R-Placer',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
